e7e436b8e81e18e000388a2409231766cd99ba20
fodnb <Daniel.Mcghee6813@gmail.com>
2017-10-26 09:43:50 -0400

    add DELETE /todos tests

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.delete('/todos/:id', (req, res) => {
 			return res.status(404).send();
 		}
 		res.status(200).send({todo});
-		console.log('removed:', todo);
+		// console.log('removed:', todo);
 	}, (e) => {
 		res.status(400).send()
 	});
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -112,7 +112,49 @@ describe('get /todos/:id', ()=>{
 		.get(`/todos/${failId}`)
 		.expect(404)
 		.end(done);
+	});
+});
 
+
+describe('DELETE /todos/:id', ()=>{
+	it('should delete todo', (done)=>{
+		var hexId = todos[1]._id.toHexString();
+		request(app)
+		.delete(`/todos/${hexId}`)
+		.expect(200)
+		.expect((res)=>{
+			expect(res.body.todo._id).toBe(hexId);
+		})
+		.end((err, res)=>{
+			if(err){
+				return done(err);
+			}
+
+			Todo.findById(hexId).then((todo)=>{
+				expect(todo).toNotExist;
+				done();
+			}).catch((e)=> done(e));
+	});
 	});
 
+	it('should return 404 if todo not found', (done) => {
+		var testObjectID = new ObjectID();
+
+		request(app)
+		.delete(`/todos/${testObjectID}`)
+		.expect(404)
+		.end(done);
+	});	
+
+	it('should return 404 if ObjectID is invalid', (done)=>{
+		const failId = "59efaa62efe4942df00dc75d";
+
+		request(app)
+		.delete(`/todos/${failId}`)
+		.expect(404)
+		.end(done);
+
+	});
 });
+
+
