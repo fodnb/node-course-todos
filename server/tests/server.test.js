09afb4398132faf79d12a2359f638c0794fc3303
fodnb <Daniel.Mcghee6813@gmail.com>
2017-12-13 18:39:32 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -250,3 +250,30 @@ describe('Post /users/login', ()=>{
 
 });
 });
+
+describe('DELETE users/me/token', () => {
+
+		it('should remove auth token on logout', (done)=>{
+
+		request(app)
+		.delete('/users/me/token')
+		.set('x-auth', users[0].tokens[0].token)
+		.expect(200)	
+		.end((err, res)=>{
+			if(err){
+				return done(err);
+			}
+			// console.log(user[0]._id);
+			User.findById(users[0]._id).then((user)=>{
+				expect(user.tokens.length).toBe(0);
+				done();
+			}).catch((e) => done(e));
+		})			
+
+
+		});
+
+
+});
+
+
